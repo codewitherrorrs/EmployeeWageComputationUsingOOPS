de030b646e9dbe460e26dce85bef6435095f377d
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-15 13:35:32 +0530

    Add : Program.cs File

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
     {
         public static void Main(string[] args)
         {
-           CalculateMonthlyWage cm = new CalculateMonthlyWage();
-            cm.montlyWage();
+            CalculateAssumeHrs ch = new CalculateAssumeHrs();
+            ch.montlyWage();
         }
     }
 }
