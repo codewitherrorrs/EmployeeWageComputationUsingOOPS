0d7884d5d4d43fc9d6656ac9be6fe811d9cd1a46
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-21 17:05:31 +0530

    Add : Program.cs File

--- Program.cs
+++ Program.cs
@@ -3,11 +3,15 @@
     public class EmployeeWage
     {
         public static void Main(string[] args)
-        { 
-            EmpWageBuiltArray empWageBuiltArray = new EmpWageBuiltArray();
-            empWageBuiltArray.addCompanyEmpWage("Dmart", 20, 2, 10);
-            empWageBuiltArray.addCompanyEmpWage("Reliance", 20, 2, 10);
-            empWageBuiltArray.computeEmpWage();
+        {
+            //EmpWageBuiltArray empWageBuiltArray = new EmpWageBuiltArray();
+            //empWageBuiltArray.addCompanyEmpWage("Dmart", 20, 2, 10);
+            //empWageBuiltArray.addCompanyEmpWage("Reliance", 20, 2, 10);
+            //empWageBuiltArray.computeEmpWage();
+
+            CompanyEmpwage companyEmpWage = new CompanyEmpwage("Dmart",20,20,100);
+            companyEmpWage.setTotalEmpwage(100);
+            companyEmpWage.tostring();
 
         }
     }
