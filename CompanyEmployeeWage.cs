4752407d4a452a46e1c4d0b77cc343ab89a2b8c3
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-21 13:42:24 +0530

    Add : Total Employee Wage Store

---
+++ CompanyEmployeeWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationUsingOOPS
+{
+    public class CompanyEmployeeWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmployeeWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        public void setTotal(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+
+        public string tostring()
+        {
+            return "Total Emp Wage for Company : " + this.company + " is " + this.totalEmpWage;
+        }
+    }
+}
