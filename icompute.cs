fbbf2ae3513e0c8da94b2033cf46e99f87858122
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-21 13:57:39 +0530

    Add : InterFace File

---
+++ icompute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationUsingOOPS
+{
+    public interface icompute
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        public void computeEmpWage();
+    }
+}
